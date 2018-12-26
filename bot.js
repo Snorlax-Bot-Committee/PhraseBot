4f8434b2910913e24a892c7081fe31185982b3ef
fyflotent <chris@chaoticlife.net>
2018-12-26 01:19:21 +0000

    Adding youtube search functionality need to abstract this

--- bot.js
+++ bot.js
@@ -10,6 +10,7 @@ const youtube_video_url="https://www.youtube.com/watch?v="
 //Sync will be issue with multi servers probably just do DB >_>
 saveRegex=/^save *["'](.*)["'] *["'](.*)["']$/;
 lmgtfyRegex=/^lmgtfy  *(.*)$/
+youtubeRegex=/^youtube *(.*)$/
 
 
 //Not in use but could be useful when 
@@ -91,19 +92,31 @@ function sendCatchPhrase(user,channel){
 
 function search_youtube(channel,query){
 	let api_url="https://www.googleapis.com/youtube/v3/search?part=snippet"
-	let youtube_address=`${api_url}maxResults=1&q=${encodeURI(query)}&key=${auth.youtube}`
+	let youtube_address=`${api_url}&maxResults=1&q=${encodeURI(query)}&key=${auth.youtube}`
 	http.get(youtube_address, (resp) => {
 		let data='';
 		resp.on('data', (chunk) => {
 			data+=chunk
 		});
 		resp.on('end', () => {
-			let query_data=JSON.parse(data)
+			let query_data={}
+			try {
+				query_data=JSON.parse(data)
+				console.log(data)
+				console.log(query_data)
+			}
+			catch (error){
+				console.log(error)
+				return 1;
+			}
 			let youtube_video=youtube_video_url+query_data.items[0].id.videoId
 			channel.send(youtube_video);
-		}
-	}).on("error", (err) =>{
-		console.log("Error: " + err.message);	
+		});
+		resp.on('error',(error)=>{
+			console.log("Error: "+error.message);
+		});
+	}).on("error", (error) =>{
+		console.log("Error: " + error.message);	
 	});
 }
 
@@ -132,6 +145,11 @@ client.on('message', msg => {
 			let match=lmgtfyRegex.exec(cmd);
 			msg.channel.send(`http://lmgtfy.com/?q=${encodeURI(match[1])}`)
 		}
+		else if (youtubeRegex.test(cmd)){
+			console.log(cmd)
+			let match=youtubeRegex.exec(cmd)
+			search_youtube(msg.channel,match[1]);
+		}
 	}
 	else if (catchPhrases[user]!=null){
 		//Set regex to catch phrase for user if exists
@@ -148,7 +166,9 @@ client.on('message', msg => {
 client.on('error',exitHandler.bind())
 //Not sure that either of the following lines do anything
 client.on('SIGINT',exitHandler.bind())
-client.on('uncaughtException',exitHandler.bind())
+client.on('uncaughtException',(error)=>{
+	console.log(error.message)
+});
 
 
 client.login(auth.token);
