2fc63ecfcf12374045f2bd3ef58c8640420eae52
fyflotent <chris@chaoticlife.net>
2018-12-28 02:27:19 +0000

    Fixed undefined issue for youtube search when first result was not a video

--- youtube.js
+++ youtube.js
@@ -6,7 +6,7 @@ const youtube_video_url="https://www.youtube.com/watch?v="
 module.exports = {
 	search_youtube: function (channel,query){
 		let api_url="https://www.googleapis.com/youtube/v3/search?part=snippet"
-		let youtube_address=`${api_url}&maxResults=1&q=${encodeURI(query)}&key=${auth.youtube}`
+		let youtube_address=`${api_url}&maxResults=15&q=${encodeURI(query)}&key=${auth.youtube}`
 		http.get(youtube_address, (resp) => {
 			let data='';
 			resp.on('data', (chunk) => {
@@ -16,13 +16,24 @@ module.exports = {
 				let query_data={}
 				try {
 					query_data=JSON.parse(data)
-					console.log(query_data.items[0].id)
 				}
 				catch (error){
 					console.log(error)
 					return 1;
 				}
-				let youtube_video=youtube_video_url+query_data.items[0].id.videoId
+				let youtube_video_id=query_data.items[0].id.videoId
+
+				for (i in query_data.items){
+					resource=query_data.items[i];
+					if( resource.id.kind==="youtube#video"){
+						youtube_video_id=resource.id.videoId
+						break;
+					}
+					else{
+						console.log(resource)
+					}
+				}
+				let youtube_video=youtube_video_url+youtube_video_id
 				channel.send(youtube_video);
 			});
 			resp.on('error',(error)=>{
